cfe1c6f5ecd2bd1be5d9bdb833dee3a75f3a088b
sabcyb <saber.ahmed.hamburg@gmail.com>
2025-10-28 17:20:16 +0100

    js logic is added

--- scripts.js
+++ scripts.js
@@ -3,29 +3,29 @@ const smallChar = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","
 const number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 const special = ["~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?","/"];
 
-const minimumLen = 15;
-
 function password_generator (){
     let password1 = "";
-    for (let i=0; i<minimumLen; i++){
+    for (let i=0; i<15; i++){
         const charDecider = Math.ceil(Math.random() * 4);
         //console.log(charDecider);
         if(charDecider === 1){
             password1 += bigChar[Math.floor(Math.random() * bigChar.length)];
         } else if (charDecider === 2){
-            password1 += smallChar[Math.ceil(Math.random() * smallChar.length)];
+            password1 += smallChar[Math.floor(Math.random() * smallChar.length)];
         } else if (charDecider === 3){
-            password1 += number[Math.ceil(Math.random() * number.length)];
+            password1 += number[Math.floor(Math.random() * number.length)];
         } else{
-            password1 += special[Math.ceil(Math.random() * special.length)];
+            password1 += special[Math.floor(Math.random() * special.length)];
         }
     }
     return password1;
 };
 
 document.querySelector("#btn").addEventListener("click",function(){
-    let pass_1 = password_generator();
-    let pass_2 = password_generator();
+    let pass_1 = "";
+    let pass_2 = "";
+    pass_1 = password_generator();
+    pass_2 = password_generator();
     document.querySelector("#pass-1").textContent = pass_1;
     document.querySelector("#pass-2").textContent = pass_2;
 })
