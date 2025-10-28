3a6c6f104eae6c3d126d99883e9fe731d566ac1d
sabcyb <saber.ahmed.hamburg@gmail.com>
2025-10-28 17:09:19 +0100

    script added

--- scripts.js
+++ scripts.js
@@ -1,2 +1,31 @@
-const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
-"/"];
+const bigChar = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+const smallChar = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+const number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+const special = ["~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?","/"];
+
+const minimumLen = 15;
+
+function password_generator (){
+    let password1 = "";
+    for (let i=0; i<minimumLen; i++){
+        const charDecider = Math.ceil(Math.random() * 4);
+        //console.log(charDecider);
+        if(charDecider === 1){
+            password1 += bigChar[Math.floor(Math.random() * bigChar.length)];
+        } else if (charDecider === 2){
+            password1 += smallChar[Math.ceil(Math.random() * smallChar.length)];
+        } else if (charDecider === 3){
+            password1 += number[Math.ceil(Math.random() * number.length)];
+        } else{
+            password1 += special[Math.ceil(Math.random() * special.length)];
+        }
+    }
+    return password1;
+};
+
+document.querySelector("#btn").addEventListener("click",function(){
+    let pass_1 = password_generator();
+    let pass_2 = password_generator();
+    document.querySelector("#pass-1").textContent = pass_1;
+    document.querySelector("#pass-2").textContent = pass_2;
+})
